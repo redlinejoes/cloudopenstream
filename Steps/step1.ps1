ad06caea18298fa46b7e4c150dc3481979e55c88
Rion Sears <rion020806@gmail.com>
2021-05-31 13:10:04 -0700

    Added securestring and Razer

--- Steps/step1.ps1
+++ Steps/step1.ps1
@@ -37,8 +37,8 @@ $InstallVideo = (Read-Host "This script will also install the Parsec GPU Updater
             $Shortcut.Arguments = "-Command `"Set-ExecutionPolicy Unrestricted; & '$PSScriptRoot\..\Setup.ps1'`" -RebootSkip"
             $Shortcut.Save()
             GetFile "https://raw.githubusercontent.com/parsec-cloud/Cloud-GPU-Updater/master/GPUUpdaterTool.ps1" "$PSScriptRoot\GPUUpdaterTool.ps1" "Cloud GPU Updater" 
-            & $PSScriptRoot\GPUUpdaterTool.ps1
-            Write-Host "In case of a restart, please use the continue shortcut on your desktop" 
+            Write-Host "In case of a restart, please use the continue shortcut on your desktop, make sure to choose no to video" -ForegroundColor Red
+            & $PSScriptRoot\GPUUpdaterTool.ps1 
 }
 
 $InstallAudio = (Read-Host "You need audio drivers, do you want VBCable? (y/n)").ToLower() -eq "y"
@@ -47,8 +47,11 @@ if($InstallAudio) {
 GetFile "https://download.vb-audio.com/Download_CABLE/VBCABLE_Driver_Pack43.zip" "$WorkDir\vbcable.zip" "VBCABLE" 
 Write-Host "Installing VBCABLE..."
 Expand-Archive -Path "$WorkDir\vbcable.zip" -DestinationPath "$WorkDir\vbcable"
-Start-Process -FilePath "$WorkDir\vbcable\VBCABLE_Setup_x64.exe" -ArgumentList "-i","-h" -NoNewWindow -Wait }
-
+Start-Process -FilePath "$WorkDir\vbcable\VBCABLE_Setup_x64.exe" -ArgumentList "-i","-h" -NoNewWindow -Wait 
+GetFile "https://drive.google.com/uc?export=download&id=18JS6ZPcFS4lU3ip0mMgrkkSf-fDdr0vN" "$WorkDir\razer.zip" "Razer Surround"
+Write-Host Installing "Razer Surround"
+Expand-Archive -Path "$WorkDir\razer.zip" -DestinationPath "$WorkDir\razer"
+Start-Process -FilePath "$WorkDir\razer\$TEMP\RazerSurroundInstaller\RzUpdateManager.exe" -ArgumentList "-i","-h" -NoNewWindow -Wait }
 
 GetFile "https://cloudopenstream.s3-us-west-2.amazonaws.com/installer_05_28.exe" "$WorkDir\openstream.exe" "Open-stream" 
 Write-Host "Installing Open-stream..."
