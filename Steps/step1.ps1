2c23b02463d995b22ddc6ae98430089064095f48
Rion Sears <rion020806@gmail.com>
2021-06-03 18:30:50 -0700

    Fixed brackets and added Razer as an option

--- Steps/step1.ps1
+++ Steps/step1.ps1
@@ -30,15 +30,14 @@ else {
 
 $Video = (Read-Host "This script will also install the Parsec GPU Updater tool, unless you already have drivers, please type y (y/n)").ToLower() -eq "y"
 
-        if($Video) {
-            $Shell = New-Object -comObject WScript.Shell
-            $Shortcut = $Shell.CreateShortcut("$Home\Desktop\Continue.lnk")
-            $Shortcut.TargetPath = "powershell.exe"
-            $Shortcut.Arguments = "-Command `"Set-ExecutionPolicy Unrestricted; & '$PSScriptRoot\..\starthere.ps1'`" -RebootSkip"
-            $Shortcut.Save()
-            GetFile "https://raw.githubusercontent.com/parsec-cloud/Cloud-GPU-Updater/master/GPUUpdaterTool.ps1" "$PSScriptRoot\GPUUpdaterTool.ps1" "Cloud GPU Updater" 
-            Write-Host "In case of a restart, please use the continue shortcut on your desktop, make sure to choose no to video" -ForegroundColor Red
-            & $PSScriptRoot\GPUUpdaterTool.ps1 
+if($Video) {
+  $Shell = New-Object -comObject WScript.Shell
+  $Shortcut = $Shell.CreateShortcut("$Home\Desktop\Continue.lnk")
+  $Shortcut.TargetPath = "powershell.exe"
+  $Shortcut.Arguments = "-Command `"Set-ExecutionPolicy Unrestricted; & '$PSScriptRoot\..\starthere.ps1'`" -RebootSkip"
+  $Shortcut.Save()
+  GetFile "https://raw.githubusercontent.com/parsec-cloud/Cloud-GPU-Updater/master/GPUUpdaterTool.ps1" "$PSScriptRoot\GPUUpdaterTool.ps1" "Cloud GPU Updater" 
+  & $PSScriptRoot\GPUUpdaterTool.ps1 
 }
 
 $Audio = (Read-Host "You need audio drivers, do you want VBCable? (y/n)").ToLower() -eq "y"
@@ -48,11 +47,16 @@ GetFile "https://download.vb-audio.com/Download_CABLE/VBCABLE_Driver_Pack43.zip"
 Write-Host "Installing VBCABLE..."
 Expand-Archive -Path "$WorkDir\vbcable.zip" -DestinationPath "$WorkDir\vbcable"
 Start-Process -FilePath "$WorkDir\vbcable\VBCABLE_Setup_x64.exe" -ArgumentList "-i","-h" -NoNewWindow -Wait 
+}
 
+$Razer = (Read-Host "Do you want to install Razer Surround? (y/n)").ToLower() -eq "y"
+
+if($Razer) {
 GetFile "https://drive.google.com/uc?export=download&id=18JS6ZPcFS4lU3ip0mMgrkkSf-fDdr0vN" "$WorkDir\razer.zip" "Razer Surround"
 Write-Host Installing "Razer Surround"
 Expand-Archive -Path "$WorkDir\razer.zip" -DestinationPath "$WorkDir\razer"
-Start-Process -FilePath "$WorkDir\razer\$TEMP\RazerSurroundInstaller\RzUpdateManager.exe" -ArgumentList "-i","-h" -NoNewWindow -Wait }
+Start-Process -FilePath "$WorkDir\razer\$TEMP\RazerSurroundInstaller\RzUpdateManager.exe" -ArgumentList "-i","-h" -NoNewWindow -Wait 
+}
 
 GetFile "https://cloudopenstream.s3-us-west-2.amazonaws.com/installer_05_28.exe" "$WorkDir\openstream.exe" "Open-stream" 
 Write-Host "Installing Open-stream..."
