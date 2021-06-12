1f480ee73112d4547d5f34446356c25ccc22c36b
Rion Sears <rion020806@gmail.com>
2021-06-12 10:48:07 -0700

    Removed line to make sure it works

--- Steps/step1.ps1
+++ Steps/step1.ps1
@@ -11,11 +11,6 @@ Function GetFile([string]$Url, [string]$Path, [string]$Name) {
     }
 }
 
-Write-Host Disabling IE Enhanced Security...
-Set-Itemproperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -name IsInstalled -value 0 -force | Out-Null
-Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 -Force | Out-Null
-Stop-Process -Name Explorer -Force
-
 $Video = (Read-Host "This script will also install the Parsec GPU Updater tool, unless you already have drivers, please type y (y/n)").ToLower() -eq "y"
 
 if($Video) {
