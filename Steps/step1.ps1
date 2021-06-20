d6dc32ef7cd04e631abcf7ad0213f90b565b5a08
Rion Sears <rion020806@gmail.com>
2021-06-19 21:29:29 -0700

    Fixed Chrome installer link

--- Steps/step1.ps1
+++ Steps/step1.ps1
@@ -1,5 +1,5 @@
 $WorkDir = "$PSScriptRoot\..\Bin"
-
+ 
 Function GetFile([string]$Url, [string]$Path, [string]$Name) {
     try {
         if(![System.IO.File]::Exists($Path)) {
@@ -10,20 +10,20 @@ Function GetFile([string]$Url, [string]$Path, [string]$Name) {
         throw "`"$Name`" download failed."
     }
 }
-
+ 
 Set-Itemproperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -name IsInstalled -value 0 -force | Out-Null
 Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 -Force | Out-Null
 Stop-Process -Name Explorer -Force
-
+ 
 $Audio = (Read-Host "You need audio drivers, do you want VBCable? (y/n)").ToLower() -eq "y"
-
+ 
 if($Audio) { 
 GetFile "https://download.vb-audio.com/Download_CABLE/VBCABLE_Driver_Pack43.zip" "$WorkDir\vbcable.zip" "VBCABLE" 
 Write-Host "Installing VBCABLE..."
 Expand-Archive -Path "$WorkDir\vbcable.zip" -DestinationPath "$WorkDir\vbcable"
 Start-Process -FilePath "$WorkDir\vbcable\VBCABLE_Setup_x64.exe" -ArgumentList "-i","-h" -NoNewWindow -Wait 
 }
-
+ 
 GetFile "https://aka.ms/vs/16/release/vc_redist.x64.exe" "$WorkDir\redist.exe" "Visual C++ Redist (2015-19)"
 Write-Host "Installing Visual Studio Redist"
 $ExitCode = (Start-Process -FilePath "$WorkDir\redist.exe" -ArgumentList "/install","/quiet","/norestart" -NoNewWindow -Wait -Passthru).ExitCode
@@ -32,21 +32,17 @@ elseif($ExitCode -eq 1638) { Write-Host "Newer version already installed." -Fore
 else { 
     throw "Installation failed (Error: $ExitCode)."
 }
-
-GetFile "https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi" "$WorkDir\chrome.msi" "Google Chrome" 
+ 
+GetFile "https://dl.google.com/tag/s/appguid/chrome/install/ChromeStandaloneSetup64.exe" "$WorkDir\chrome.exe" "Google Chrome" 
 Write-Host "Installing Chrome..."
-$ExitCode = (Start-Process -FilePath "C:\Windows\System32\msiexec.exe" -ArgumentList '/qn /i "$WorkDir\chrome.msi"' -NoNewWindow -Wait -Passthru).ExitCode
-if($ExitCode -eq 0) { Write-Host "Installed." -ForegroundColor Green }
-else { 
-    throw "Installation failed (Error: $ExitCode)."
-}
-
+Start-Process -FilePath "$WorkDir\chrome.exe" -NoNewWindow -Wait -Passthru
+ 
 GetFile "https://cloudopenstream.s3-us-west-2.amazonaws.com/installer_05_28.exe" "$WorkDir\openstream.exe" "Open-stream" 
 Write-Host "Installing Open-stream..."
-Start-Process -FilePath "$WorkDir\openstream.exe"
-
+Start-Process -FilePath "$WorkDir\openstream.exe" -Wait
+ 
 $Video = (Read-Host "Now it's time for GPU drivers, unless you already have drivers, please use the tool (y/n)").ToLower() -eq "y"
-
+ 
 if($Video) {
   $Shell = New-Object -comObject WScript.Shell
   $Shortcut = $Shell.CreateShortcut("$Home\Desktop\Continue.lnk")
